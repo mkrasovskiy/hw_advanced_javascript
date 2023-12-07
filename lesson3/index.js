482f36f3829a657978cd490a5d209466ffb12f98
Maks <kpasowsky@gmail.com>
2023-12-08 00:17:56 +0300

    lesson3

--- lesson3/index.js
+++ lesson3/index.js
@@ -1,18 +1,17 @@
 const select = document.querySelector('#products');
 const textarea = document.querySelector('#review');
+
 document.querySelector('button').addEventListener('click', function() {
-  const selectSave = select.value;
-  const text = textarea.value;
-    
+    const selectSave = select.value;
+    const text = textarea.value;
     let storedValues;
-    if (localStorage.getItem(selectSave) === null) {
-        storedValues = [];
-    } else {
-        storedValues = JSON.parse(localStorage.getItem(selectSave));
-    }
+        if (localStorage.getItem(selectSave) === null) {
+            storedValues = [];
+        } else {
+            storedValues = JSON.parse(localStorage.getItem(selectSave));
+        }
     
     storedValues.push(text);
-    
     localStorage.setItem(selectSave, JSON.stringify(storedValues));
     textarea.value = "";
 });
