5b5ff9360bbeacc7dddd4ad184431a0e94652731
Maks <kpasowsky@gmail.com>
2023-12-09 17:17:28 +0300

    lesson3

--- lesson3/index.js
+++ lesson3/index.js
@@ -10,19 +10,37 @@ document.querySelector('.button').addEventListener('click', function() {
         } else {
             storedValues = JSON.parse(localStorage.getItem(selectSave));
         }
-    
     storedValues.push(text);
     localStorage.setItem(selectSave, JSON.stringify(storedValues));
     textarea.value = "";
-
-
-    document.querySelector('.btn').addEventListener('click', function() {
-        const selectValue = document.getElementById('products').value;
-        const storedData = JSON.parse(localStorage.getItem(selectValue));
-        const divElement = document.getElementById('download-reviews');
-        divElement.innerHTML = storedData.map(n => `<p>${n}</p>`).join('');
-    });
 });
-
-
-
+document.querySelector('.h2').addEventListener('click', function() {
+    const keys = Object.keys(localStorage);
+    keys.forEach((key, index) => {
+        const divEl = document.getElementById('all-reviews');
+        const btn = document.createElement('button');
+        btn.className = ('button-rev');
+        btn.textContent = key;
+        const divElem = document.createElement('div');
+        divElem.className = ('active');
+        const values = JSON.parse(localStorage.getItem(key));
+        divElem.innerHTML = values.map(e => `<p id="reviewId">${e}</p> <button class="remove">Удалить</button>`).join('');
+        divEl.append(btn);
+        divEl.append(divElem);
+    
+        btn.addEventListener('click', function() {
+            divElem.classList.toggle('active');
+        });
+        const buttonEl = document.querySelector('.remove');
+        buttonEl.addEventListener('click', function() {    
+            //не могу понять как удалить из  localStorage..
+            //попробывал так, не получается,если вставляю "key", то удаляются все значения вообще        
+            // values.filter((el) => {
+            //     if(el !== index) {
+            //         localStorage.removeItem(el);
+            //     }
+            // });            
+        });
+    });
+    
+});
